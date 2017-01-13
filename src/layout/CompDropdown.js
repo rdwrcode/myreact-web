bede65c106e3b1f475781285c5ceb675a287e504
rdwrcode <rdwrcode@gmail.com>
2017-01-12 19:02:30 -0700

    layout in progress

--- src/layout/CompDropdown.js
+++ src/layout/CompDropdown.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import './CompDropdown.css';
 
+/*
+ * <CompDropdown options={options} defaultIndex='0' cb={callback}/>
+ * options: an array of strings which has all the choices
+ * defaultIndex: the index of the array as a string, starting from '0'
+ * cb: the callback function pointer
+ * 
+ * To use the destructuring, construct an object first as follows:
+ 
+  const params = {
+    options: ['a', 'b', 'c'],
+    defaultIndex: '0',
+    cb: callbackFunction  
+  } 
+ 
+ * Then it is easy to use CompDropdown like this:
+
+  <CompDropdown {...params}/>
+
+ * All styles can be specified in CompDropdown.css with React.
+ *  
+ */
+
 class CompDropdown extends Component {
   constructor(props) {
     super(props);
