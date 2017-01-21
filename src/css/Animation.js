70402a5fc346fa7b728dfa23c28f4f9489026e1a
rdwrcode <rdwrcode@gmail.com>
2017-01-21 14:21:28 -0700

    add two exercises

--- src/css/Animation.js
+++ src/css/Animation.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { css } from 'glamor';
 import './Animation.css';
 
+const nameList = ['bounce', 'pulse', 'rubberband', 'wobble', 'flip', 'hinge'];
+
 class Animation extends Component {
   render() {
+
     return (
-      <div className="Animation">
-        Animation
+      <div className='Animation'>
+        <div className='box1'>
+          Hover over the box to see the animation.
+        </div>
+        <div className='container'>
+        {
+          nameList.map((e, index) => (
+            <div key={index+100} className='box'>
+              <div key={index+1000} className={'text animated '+e}>{e}</div>
+            </div>
+          ))
+        }
+        </div>
       </div>
     );
   }
